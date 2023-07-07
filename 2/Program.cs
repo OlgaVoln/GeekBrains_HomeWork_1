fb1ebebcd4af57e0e7d7ba8b895fa952d79c1c49
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-07 19:48:32 +0300

    ДЗ 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = int.Parse(Console.ReadLine());
+int numberMax = 0;
+
+if (numberA > numberB)
+{
+    numberMax = numberA;
+}
+else
+{
+    numberMax = numberB;
+}
+
+if (numberMax < numberC)
+{
+    numberMax = numberC;
+}
+
+Console.WriteLine(numberMax);
