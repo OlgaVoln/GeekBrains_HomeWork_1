b3e2305469c1eb30b7bfcfe030c3bb21e3aa7181
Olga Volosenko <olgavolosenko@gmail.com>
2023-07-07 19:14:57 +0300

    ДЗ номер 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} больше {numberB}");
+}
+if (numberA < numberB)
+{
+    Console.WriteLine($"Число {numberA} меньше {numberB}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
